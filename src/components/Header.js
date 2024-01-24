2a470058b9af11d1bcf499ba8dae36212fa7bde2
kiboma <omayioben@gmail.com>
2024-01-24 19:37:19 +0300

    Style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,14 +7,14 @@ export const Header = () => {
     <div className="flex justify-around items-center flex-wrap mt-5">
         <Link to='/' className="flex flex-wrap items-center gap-4" >
             <img className="w-24 rounded" src={Logo} alt="FoodApp" />
-            <p className="text-2xl" >Food ChapChap</p>
+            <p className="text-4xl" >Food ChapChap</p>
         </Link>
-        <div>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='cart' >Cart</NavLink>
+        <div className="flex gap-10 text-2xl">
+            <NavLink to='/' className="m-2 p-2">Home</NavLink>
+            <NavLink to='cart' className="m-2 p-2" >Cart</NavLink>
         </div>
         <Link to='cart'>
-            <p>Cart: 5</p>
+            <p className="text-2xl font-bold">Cart: 5</p>
         </Link>
     </div>
   )
