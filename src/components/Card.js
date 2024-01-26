8fa8d8af7b71bf2ea7a49dd02cac4d6cd9918347
kiboma <omayioben@gmail.com>
2024-01-26 17:41:32 +0300

    Debug enslint errors

--- src/components/Card.js
+++ src/components/Card.js
@@ -20,11 +20,11 @@ export const Card = ({food}) => {
 
   return (
     <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
+        <a href="/">
             <img className="p-8 rounded-t-lg" src={image} alt={name} />
         </a>
         <div className="px-5 pb-5">
-            <a href="#">
+            <a href="/">
                 <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{name}</h5>
             </a>
 
