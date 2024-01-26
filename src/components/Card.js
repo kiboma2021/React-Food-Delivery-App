f5165ecc82373f1214d4b4d1669bcd849b5c9cdf
kiboma <omayioben@gmail.com>
2024-01-26 17:11:20 +0300

    Check and remoce items from home page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
 import { useCart } from "../context/CartContext";
 
 export const Card = ({food}) => {
-  const { name, price, image } = food;
-  const { addToCart } = useCart();
+  const { id, name, price, image } = food;
+  const { cartList,addToCart,removeItemFromCart } = useCart();
+
+  const [isInCart, setIsInCart] = useState(false);
+
+  useEffect(() =>{
+    const checkItem = cartList.find(item => item.id === id);
+    if (checkItem) {
+        setIsInCart(true);
+      } else {
+        setIsInCart(false);
+      }
+
+  },[cartList,id])
   
+
   return (
     <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
@@ -16,7 +30,11 @@ export const Card = ({food}) => {
 
             <div className="flex items-center justify-between">
                 <span className="text-xl text-gray-900 dark:text-white">${price}</span>
-                <span onClick={()=>addToCart(food)} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</span>
+                { isInCart ? (
+                    <span onClick={()=>removeItemFromCart(food)} className='bg-red-600 p-2 m-2 rounded text-white'>Remove</span>
+                ): (
+                    <span onClick={()=>addToCart(food)} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</span>
+                )}        
             </div>
         </div>
     </div>
